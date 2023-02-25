461361cc386d15271451af5d07297b42f7f7dea0
Jaydeep-07 <jaydeepvpatil225@gmail.com>
2023-02-25 13:21:37 +0530

    Add project files.

---
+++ ELSADemo/Controllers/ELSADashboardController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ELSADemo.Controllers
+{
+    public class ELSADashboardController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
